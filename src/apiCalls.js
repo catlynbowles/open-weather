21d22e2d02d2562d8e791462307d36b486c4d243
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-10-06 09:30:23 -0600

    change fetch to https for codeSandbox

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,11 @@
 export const getCityData = (city, state) => {
-  return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=1&appid=${process.env.REACT_APP_API_KEY}`)
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error('Error')
-      }
-    })
-}
+  return fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=1&appid=${process.env.REACT_APP_API_KEY}`
+  ).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Error");
+    }
+  });
+};
