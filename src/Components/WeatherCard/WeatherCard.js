1df4edab4a3eb3f03883b1edf05ac1539de59d9d
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-10-06 10:52:40 -0600

    Add icons to WeatherCard

--- src/Components/WeatherCard/WeatherCard.js
+++ src/Components/WeatherCard/WeatherCard.js
@@ -1,7 +1,13 @@
 import { useEffect } from "react"
 
-const WeatherCard = () => {
-
+const WeatherCard = ({name, temp, icon, id}) => {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>{temp}</p>
+      <img src={`http://openweathermap.org/img/wn/${icon}.png`}></img>
+    </div>
+  )
 }
 
 export default WeatherCard
--- src/Components/WeatherGrid/WeatherGrid.js
+++ src/Components/WeatherGrid/WeatherGrid.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getLocalWeather } from "../../apiCalls"
+import WeatherCard from "../WeatherCard/WeatherCard"
 
 const WeatherGrid = ({cityCoordinates}) => {
   const [cityDetails, setCityDetails] = useState([])
@@ -7,10 +8,25 @@ const WeatherGrid = ({cityCoordinates}) => {
   useEffect(() => {
     Promise.all(cityCoordinates.map(city => getLocalWeather(city.lat.toFixed(2), city.lon.toFixed(2))))
       .then(data => setCityDetails(data))
-  }, [])
+      console.log('itran')
+  }, [cityCoordinates])
+
+  const generateWeatherCards = (weatherStatistics) => {
+    return weatherStatistics.map(city => {
+      return (
+        <WeatherCard 
+          id={city.id}
+          key={city.id}
+          name={`${city.name}`}
+          temp={city.main.temp}
+          icon={city.weather[0].icon}
+        />
+      )
+    })
+  }
 
   return (
-    <div>hi</div>
+    <div>{cityDetails.length ? generateWeatherCards(cityDetails) : null}</div>
   )
 }
 
