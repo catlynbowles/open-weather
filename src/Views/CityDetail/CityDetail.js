167b054a9e6831b223e0dd3bcad5cd22b6a88b74
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-10-06 15:59:23 -0600

    Add props for modal

--- src/Views/CityDetail/CityDetail.js
+++ src/Views/CityDetail/CityDetail.js
@@ -3,7 +3,13 @@ import Modal from "../../Components/Modal/Modal"
 const CityDetail = ({selectedCity}) => {
   return (
     <Modal
-      
+      description={selectedCity.weather[0].main}
+      temp={selectedCity.main.temp}
+      humidity={selectedCity.main.humidity}
+      pressure={selectedCity.main.pressure}
+      key={selectedCity.id}
+      id={selectedCity.id}
+      name={selectedCity.name}
     /> 
   )
 }
