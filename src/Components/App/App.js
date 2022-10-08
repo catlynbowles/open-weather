8bf255740d3256e1a5109c0033080bf0297dadba
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-10-07 19:59:25 -0600

    Update fetch calls to hurricane specific cities

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,7 +8,7 @@ function App() {
   const [cityCoordinates, setCityCoordinates] = useState([])
 
   useEffect(() => {
-    Promise.all([getCityCoordinate('Las Vegas', 'Nevada'), getCityCoordinate('Anchorage', 'Alaska'), getCityCoordinate('Honolulu', 'Hawaii'), getCityCoordinate('New York', 'New York'), getCityCoordinate('Los Angeles', 'California')])
+    Promise.all([getCityCoordinate('Honolulu', 'Hawaii'), getCityCoordinate('New York', 'New York'), getCityCoordinate('Houston', 'Texas'), getCityCoordinate('New Orleans', 'Louisiana'), getCityCoordinate('Miami', 'Florida')])
       .then(data => setCityCoordinates(data.flat(1)))
   }, [])
 
