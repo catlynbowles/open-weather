4eabec81451a07aae8d3de98a0b68d1bcbd00cb4
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-06-29 13:49:39 -0600

    update router, add correct routes for urls

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,31 +1,43 @@
-import './App.scss';
-import { useEffect, useState } from 'react';
-import { getCityCoordinate } from '../../apiCalls';
-import Homepage from '../../Views/Homepage/Homepage';
-import Header from '../Header/Header';
-import Error from '../Error/Error';
+import "./App.scss";
+import { useEffect, useState } from "react";
+import { getCityCoordinate } from "../../apiCalls";
+import Homepage from "../../Views/Homepage/Homepage";
+import Header from "../Header/Header";
+import Error from "../Error/Error";
 
 function App() {
-  const [cityCoordinates, setCityCoordinates] = useState([])
-  const [error, setError] = useState('')
+  const [cityCoordinates, setCityCoordinates] = useState([]);
+  const [error, setError] = useState("");
   const [locations] = useState([
-    { city: 'Honolulu', state: 'Hawaii' },
-    { city: 'New York', state: 'New York' },
-    { city: 'Las Vegas', state: 'Nevada' },
-    { city: 'Anchorage', state: 'Alaska' },
-    { city: 'Portland', state: 'Oregon' }
-  ])
+    { city: "Honolulu", state: "Hawaii" },
+    { city: "New York", state: "New York" },
+    { city: "Las Vegas", state: "Nevada" },
+    { city: "Anchorage", state: "Alaska" },
+    { city: "Portland", state: "Oregon" },
+  ]);
 
   useEffect(() => {
-    Promise.allSettled(locations.map(loc => getCityCoordinate(loc.city, loc.state)))
-      .then(data => setCityCoordinates(data.filter(loc => loc.status === 'fulfilled').map(loc => loc.value).flat(1)))
-      .catch(err => setError(err))
-  }, [])
+    Promise.allSettled(
+      locations.map((loc) => getCityCoordinate(loc.city, loc.state))
+    )
+      .then((data) =>
+        setCityCoordinates(
+          data
+            .filter((loc) => loc.status === "fulfilled")
+            .map((loc) => loc.value)
+            .flat(1)
+        )
+      )
+      .catch((err) => setError(err));
+  }, []);
 
   return (
-    <div className='App'>
-      <Header />
-      {error ? <Error error={error} /> : <Homepage cityCoordinates={cityCoordinates} />}
+    <div className="App">
+      {error ? (
+        <Error error={error} />
+      ) : (
+        <Homepage cityCoordinates={cityCoordinates} />
+      )}
     </div>
   );
 }
